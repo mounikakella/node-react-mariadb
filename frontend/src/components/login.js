8467162aff6efe1709096ae1e7635313be198df9
Mounika Kella <mounikakella999@gmail.com>
2021-06-10 15:55:54 +0000

    Added fixings

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -13,28 +13,29 @@ const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-
+  const [submit, setSubmit] = useState(false);
   async function handleSubmit() {
-    const res = await fetch(`${config.BACKEND_BASE_URL}/users/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    });
-    const body = await res.json();
-    if (res.status === 403) {
-      setError("Authentication error");
-    } else if (res.status !== 200) {
-      setError("Something is wrong");
-    } else {
-      console.log("redirect to profile");
-      console.log(body);
-      props.history.push("/profile");
-      localStorage.setItem("user", JSON.stringify(body[0]));
+    setSubmit(true);
+    if (email && password) {
+      const res = await fetch(`${config.BACKEND_BASE_URL}/users/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      if (res.status === 403) {
+        setError("Authentication Failed");
+      } else if (res.status !== 200) {
+        setError("Something is wrong");
+      } else {
+        const body = await res.json();
+        props.history.push("/profile");
+        localStorage.setItem("user", JSON.stringify(body[0]));
+      }
     }
   }
 
@@ -67,6 +68,7 @@ const Login = (props) => {
         >
           <Box m={1}>
             <TextField
+              error={submit && email === "" ? true : false}
               id="email"
               label="Email"
               variant="outlined"
@@ -75,6 +77,8 @@ const Login = (props) => {
           </Box>
           <Box m={1}>
             <TextField
+              error={submit && password === "" ? true : false}
+              type="password"
               id="password"
               label="Password"
               variant="outlined"
